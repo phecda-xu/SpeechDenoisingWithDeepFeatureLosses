c2ae9b5b5d088a75685ef234053e1aba7743fc2c
phecda-xu <viaxu910515@163.com>
2019-11-14 16:58:59 +0800

    update

--- covert_wav.sh
+++ covert_wav.sh
@@ -3,6 +3,7 @@
 cd dataset_tmp/__background__/
 
 mkdir -p "../../dataset/__background__/"
+echo "process __background__"
 for f in *.wav;
     do
     sox "$f" -e float -b 32 "../../dataset/__background__/$f" rate -v -I 16000
@@ -12,6 +13,7 @@ cd ../..
 cd dataset_tmp/trainset_clean/
 for i in */;
     do
+    echo $i
     mkdir -p "../../dataset/trainset_clean/$i"
     cd "$i"
     for f in *.wav;
@@ -26,6 +28,7 @@ cd ../..
 cd dataset_tmp/valset_clean/
 for i in */;
     do
+    echo $i
     mkdir -p "../../dataset/valset_clean/$i"
     cd "$i"
     for f in *.wav;
@@ -39,6 +42,7 @@ cd ../..
 cd dataset_tmp/testset_clean/
 for i in */;
     do
+    echo $i
     mkdir -p "../../dataset/testset_clean/$i"
     cd "$i"
     for f in *.wav;
