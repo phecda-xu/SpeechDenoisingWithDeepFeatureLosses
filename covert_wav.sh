fe19aa6dcb7818f1bc6e8808daa92f2a7ec62c73
phecda-xu <viaxu910515@163.com>
2019-11-14 16:28:43 +0800

    update

---
+++ covert_wav.sh
@@ -0,0 +1,21 @@
+#!/usr/bin
+
+mkdir -p dataset
+
+cd dataset_tmp
+for d in */;
+    do
+    echo 111$d
+    for i in $d*/;
+        do
+        echo 222$i
+        mkdir -p "../dataset/$i"
+        cd "$i"
+        for f in *.wav; 
+            do
+            # echo 333"../../../dataset/$i$f"
+            sox "$f" -e float -b 32 "../../../dataset/$i$f" rate -v -I 16000
+            done
+        cd ../..
+        done
+    done
