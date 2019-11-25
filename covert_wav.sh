821ee5ef32b3f36a73c88ff01e8d80488c81f550
phecda-xu <viaxu910515@163.com>
2019-11-25 18:58:15 +0800

    update

--- covert_wav.sh
+++ covert_wav.sh
@@ -10,44 +10,45 @@ for f in *.wav;
     done
 cd ../..
 
-cd dataset_tmp/trainset_clean/
-for i in */;
-    do
-    echo "trainset_clean/$i"
-    mkdir -p "../../dataset/trainset_clean/$i"
-    cd "$i"
-    for f in *.wav;
-        do
-        sox "$f" -e float -b 32 "../../../dataset/trainset_clean/$i$f" rate -v -I 16000
-        done
-    cd ..
-    done
 
-
-cd ../..
-cd dataset_tmp/valset_clean/
-for i in */;
-    do
-    echo "valset_clean/$i"
-    mkdir -p "../../dataset/valset_clean/$i"
-    cd "$i"
-    for f in *.wav;
-        do
-        sox "$f" -e float -b 32 "../../../dataset/valset_clean/$i$f" rate -v -I 16000
-        done
-    cd ..
-    done
-
-cd ../..
-cd dataset_tmp/testset_clean/
-for i in */;
-    do
-    echo "testset_clean/$i"
-    mkdir -p "../../dataset/testset_clean/$i"
-    cd "$i"
-    for f in *.wav;
-        do
-        sox "$f" -e float -b 32 "../../../dataset/testset_clean/$i$f" rate -v -I 16000
-        done
-    cd ..
-    done
+#cd dataset_tmp/trainset_clean/
+#for i in */;
+#    do
+#    echo "trainset_clean/$i"
+#    mkdir -p "../../dataset/trainset_clean/$i"
+#    cd "$i"
+#    for f in *.wav;
+#        do
+#        sox "$f" -e float -b 32 "../../../dataset/trainset_clean/$i$f" rate -v -I 16000
+#        done
+#    cd ..
+#    done
+#
+#
+#cd ../..
+#cd dataset_tmp/valset_clean/
+#for i in */;
+#    do
+#    echo "valset_clean/$i"
+#    mkdir -p "../../dataset/valset_clean/$i"
+#    cd "$i"
+#    for f in *.wav;
+#        do
+#        sox "$f" -e float -b 32 "../../../dataset/valset_clean/$i$f" rate -v -I 16000
+#        done
+#    cd ..
+#    done
+#
+#cd ../..
+#cd dataset_tmp/testset_clean/
+#for i in */;
+#    do
+#    echo "testset_clean/$i"
+#    mkdir -p "../../dataset/testset_clean/$i"
+#    cd "$i"
+#    for f in *.wav;
+#        do
+#        sox "$f" -e float -b 32 "../../../dataset/testset_clean/$i$f" rate -v -I 16000
+#        done
+#    cd ..
+#    done
