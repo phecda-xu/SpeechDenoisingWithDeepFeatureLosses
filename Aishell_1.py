d92aa577fce310381fadb5ba53e2aa6c057c2232
phecda-xu <viaxu910515@163.com>
2019-11-25 15:45:07 +0800

    update

--- Aishell_1.py
+++ Aishell_1.py
@@ -6,66 +6,87 @@ import soundfile as sf
 from tqdm import tqdm
 
 
-def load_data(speech_path, bk_path):
-    speech_wav = {}
-    bk_list = []
-    for bk in tqdm(os.listdir(bk_path)):
-        bk_list.append(bk_path + '/{}'.format(bk))
+def load_data(speech_path, noise_path):
+    speech_list = []
+    noise_list = []
+    for bk in tqdm(os.listdir(noise_path)):
+        noise_list.append(noise_path + '/{}'.format(bk))
     for person in tqdm(os.listdir(speech_path)):
         wav_list = []
         for wav in os.listdir(speech_path + '/{}'.format(person)):
-            wav_list.append(speech_path + '/{}/{}'.format(person,wav))
-        speech_wav[person] = wav_list
-    return speech_wav, bk_list
+            wav_list.append(speech_path + '/{}/{}'.format(person, wav))
+        speech_list.extend(wav_list)
+    return speech_list, noise_list
 
 
-def add_noise(speech_wav_dic, bk_wav_list, wav_save_path, setname):
-    wav_dic = {}
-    for speaker in speech_wav_dic.keys():
-        wav_list = []
-        bk_ground_sig, sr = sf.read(random.choice(bk_wav_list))
-        exists = os.path.isdir(os.path.join(wav_save_path, speaker))
-        if exists:
-            print("wav saved in {}".format(wav_save_path + speaker))
+def speech_join_noise(speech_list, noise_list):
+    speech_size = len(speech_list)
+    noise_size = len(noise_list)
+    speech_noise_list = []
+    for i in range(speech_size):
+        speech_noise_dic = {}
+        if i >= noise_size:
+            j = i - noise_size*int(i/noise_size)
         else:
-            print("build {}".format(os.path.join(wav_save_path, speaker)))
-            os.makedirs(os.path.join(wav_save_path, speaker))
-        print('processing speaker {}'.format(speaker))
-        for wav in speech_wav_dic[speaker]:
-            # speech signal
-            wav_name = os.path.basename(wav)
-            sig, sr = sf.read(wav)
-            if len(sig) < 16000:
-                continue
-            #
-            wav_list.append(wav_name)
-            #
-            p_sig = np.sum(abs(sig) ** 2)
-            if p_sig <= 10:
-                SNR = round(np.random.uniform(20, 20), 2)
-            elif 10 < p_sig <= 50:
-                SNR = round(np.random.uniform(15, 20), 2)
-            elif 50 < p_sig <= 100:
-                SNR = round(np.random.uniform(10, 15), 2)
-            elif 100 < p_sig <= 200:
-                SNR = round(np.random.uniform(5, 10), 2)
-            else:
-                SNR = round(np.random.uniform(0, 5), 2)
-            background_volume = p_sig / 10 ** (SNR / 10)
-            # background signal
-            length = len(sig)
-            end = len(bk_ground_sig)
-            start = random.randint(0, max(0, end - length))
-            background_buffer = bk_ground_sig[start: start+length]
-            background_buffer = np.sqrt(background_volume / p_sig) * background_buffer
-            # add noise
-            new_wav = background_buffer + sig
-            # saving
-            sf.write(os.path.join(wav_save_path, speaker, wav_name),new_wav,sr,subtype='PCM_32')
-        wav_dic[speaker] = wav_list
-    json_file = '{}/{}.json'.format(os.path.dirname(wav_save_path), setname)
+            j = i
+        speech_noise_dic["clean_speech"] = speech_list[i]
+        speech_noise_dic["noise"] = noise_list[j]
+        speech_noise_list.append(speech_noise_dic)
+    return speech_noise_list
+
+
+def add_noise(speech_noise_list, setname):
+    noisy_speech_list = []
+    speech_dic = {}
+    for i in tqdm(speech_noise_list):
+        clean_speech = i["clean_speech"]
+        speaker = clean_speech.split('/')[-2]
+        speech_dic[speaker] = []
+    for i in tqdm(speech_noise_list):
+        clean_speech = i["clean_speech"]
+        speaker = clean_speech.split('/')[-2]
+        noise = i["noise"]
+        wav_save_path = os.path.dirname(clean_speech).replace('clean', 'noisy')
+        wav_name = os.path.basename(clean_speech)
+        if not os.path.exists(wav_save_path):
+            os.makedirs(wav_save_path)
+        noisy_speech = os.path.join(wav_save_path, wav_name)
+        sig, sr = sf.read(clean_speech)
+        bk_ground_sig, sr = sf.read(noise)
+        if len(sig) < 16000:
+            continue
+        #
+        speech_dic[speaker].append(wav_name)
+        noisy_speech_list.append(noisy_speech)
+        #
+        p_sig = np.sum(abs(sig) ** 2)
+        if p_sig <= 10:
+            SNR = round(np.random.uniform(20, 20), 2)
+        elif 10 < p_sig <= 50:
+            SNR = round(np.random.uniform(15, 20), 2)
+        elif 50 < p_sig <= 100:
+            SNR = round(np.random.uniform(10, 15), 2)
+        elif 100 < p_sig <= 200:
+            SNR = round(np.random.uniform(5, 10), 2)
+        else:
+            SNR = round(np.random.uniform(0, 5), 2)
+        background_volume = p_sig / 10 ** (SNR / 10)
+        # background signal
+        length = len(sig)
+        end = len(bk_ground_sig)
+        if length > end:
+            bk_ground_sig = np.tile(bk_ground_sig, np.ceil(length/end))
+        start = random.randint(0, max(0, end - length))
+        background_buffer = bk_ground_sig[start: start + length]
+        background_buffer = np.sqrt(background_volume / p_sig) * background_buffer
+        # add noise
+        new_wav = background_buffer + sig
+        # saving
+        sf.write(noisy_speech, new_wav, sr, subtype='PCM_32')
+
+    json_file = '{}/{}.json'.format('dataset', setname)
     with open(json_file, 'w') as f:
-        json.dump(wav_dic, f)
+        json.dump(speech_dic, f)
     return json_file
 
 
@@ -84,10 +105,11 @@ def main():
     bk_path = 'dataset/__background__'
     for i in ['trainset', 'valset', 'testset']:  #
         speech_path = 'dataset/{}_clean'.format(i)
-        speech_wav, bk_list = load_data(speech_path, bk_path)
+        speech_list, bk_list = load_data(speech_path, bk_path)
         #
-        wav_save_path = 'dataset/{}_noisy'.format(i)
-        json_file = add_noise(speech_wav, bk_list, wav_save_path, i)
+        speech_noise_list = speech_join_noise(speech_list, bk_list)
+
+        json_file = add_noise(speech_noise_list, i)
         #
         wav_list = read_json(json_file)
         len(wav_list)
